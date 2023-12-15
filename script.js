6a3316595dfa897192875bafd2705cb4b9d509d1
Hicham Amroussi <hicham.amroussi@gmail.com>
2023-12-15 19:57:46 +0100

    Changed the language to FR

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 //Variables
 const quizData = [
   {
-    question: "كم عدد ألقاب كأس الأمم الأفريقية الحائزة عليها الجزائر؟",
-    a: "صفر لقب",
-    b: "أربع ألقاب",
-    c: "لقبين",
-    d: "لقب واحد",
+    question: "Combien de titres de Coupe d'Afrique des Nations l'Algérie a-t-elle remportés ?",
+    a: "Aucun",
+    b: "Quatre titre",
+    c: "Deux titre",
+    d: "Un seul titre",
     correct: "c",
     background: "https://i.imgur.com/kOTqrGR.jpeg"
   },
   {
-    question: "من هو المدرب الذي فاز بكأس افريقيا 1990؟",
-    a: "إسماعيل خباطو",
-    b: "عبد الحميد كرمالي",
-    c: "رابح سعدان",
-    d: "رشيد مخلوفي",
+    question: "Qui est l'entraîneur qui a remporté la Coupe d'Afrique 1990 ?",
+    a: "Smaïl Khabatou",
+    b: "Abdelhamid Kermali",
+    c: "Rabah Saâdane",
+    d: "Rachid Mekhloufi",
     correct: "b",
     background: "https://i.imgur.com/EAi2wuu.jpeg"
   },
   {
-    question: "كم من مرة في تاريخها شاركت الجزائر في كأس افريقيا؟",
-    a: "ثمان مرات",
-    b: "عشر مرات",
-    c: "خمس عشرة مرة",
-    d: "تسعة عشر مرة",
+    question: "Combien de fois dans son histoire l’Algérie a-t-elle participé à la Coupe d’Afrique ?",
+    a: "Huit fois",
+    b: "Dix fois",
+    c: "Quinze fois",
+    d: "Dix neuf fois",
     correct: "d",
     background: "https://media.thestar.com.my/Prod/44603895-4BFC-4361-B6DE-8BD48B493D8D"
   },
   {
-    question: "متى كانت أول مشاركة في كأس افريقيا؟",
+    question: "Quand a eu lieu la première participation à la Coupe d'Afrique ?",
     a: "1962",
     b: "1965",
     c: "1968",
@@ -37,56 +37,56 @@ const quizData = [
     background: "https://i.imgur.com/3Za7p3L.jpeg"
   },
   {
-    question: "أي لاعب أكثر تسجيل للاهداف في هذه المنافسة؟",
-    a: "رياض محرز",
-    b: "سفيان فغولي",
-    c: "إسلام سليماني",
-    d: "رابح ماجر",
+    question: "Quel est le meilleur buteur à la CAN dans l'histoire de l'Algérie ?",
+    a: "Riyad Mahrez",
+    b: "Sofiane Feghouli",
+    c: "Islam Slimani",
+    d: "Rabah Madjer",
     correct: "a",
     background: "https://i.imgur.com/RtUXsbJ.jpeg"
   },
   {
-    question: "أين نظمت أول طبعة فاز بها المنتخب الوطني الجزائري؟",
-    a: "إثيوبيا",
-    b: "مصر",
-    c: "تونس",
-    d: "الجزائر",
+    question: "Où s'est déroulée la première CAN remportée par l'équipe nationale algérienne ?",
+    a: "Éthiopie",
+    b: "Égypte",
+    c: "Tunisie",
+    d: "Algérie",
     correct: "d",
     background: "https://i.imgur.com/WSmU5C7.jpeg"
   },
 {
-    question: "ضد من كان هدف حسين عشيو الاسطوري في كأس أفريقيا 2004 بتونس؟",
-    a: "المغرب",
-    b: "تونس",
-    c: "مصر",
-    d: "مالي",
+    question: "Contre qui Houcine Achiou a-t-il inscrit son but historique lors de la CAN 2004 organisée en Tunisie ?",
+    a: "Maroc",
+    b: "Tunisie",
+    c: "Égypte",
+    d: "Mali",
     correct: "c",
     background: "https://i.imgur.com/C3d9PMX.jpeg"
   },
 {
-    question: "أي دور أقصيت فيه الجزائر عام 2004؟",
-    a: "النهائي",
-    b: "ربع النهائي",
-    c: "نصف النهائي",
-    d: "مرحلة المجموعات",
+    question: "À quel tour l'Algérie a-t-elle été éliminé en 2004 ?",
+    a: "Finale",
+    b: "Quart de finale",
+    c: "demi-finales",
+    d: "Phase de groupes",
     correct: "b",
     background: "https://i.imgur.com/NgAqDWE.jpeg"
   },
 {
-    question: "في أي دقيقة سجل بوقرة هدف التعادل في الربع النهائي المجنون ضد فريق ساحل العاج عام 2010؟",
-    a: "الدقيقة 80",
-    b: "الدقيقة 70",
-    c: "الدقيقة 93",
-    d: "الدقيقة 91",
+    question: "A quelle minute Bougherra a-t-il égalisé lors du quart de finale fou contre la Côte d'Ivoire en 2010 ?",
+    a: "80ème minute",
+    b: "70ème minute",
+    c: "93ème minute",
+    d: "91ème minute",
     correct: "d",
     background: "https://i.imgur.com/0PW0L39.jpg"
   },
 {
-    question: "أي جزائري نال لقب أحسن لاعب في دورة 2019؟",
-    a: "رياض محرز",
-    b: "إسماعيل بن ناصر",
-    c: "يوسف بلايلي",
-    d: "بغداد بونجاح",
+    question: "Quel joueur Algérien a remporté le titre de meilleur joueur de la CAN 2019",
+    a: "Riyad Mahrez",
+    b: "Ismaël Bennacer",
+    c: "Youcef Belaïli",
+    d: "Baghdad Bounedjah",
     correct: "b",
     background: "https://i.imgur.com/2Mjlw20.jpeg"
   }
@@ -182,9 +182,14 @@ function setNextQuestion() {
         background.style.justifyContent = "center";
         background.innerHTML = `
           <hr style="border: none; height: 2px; width:10%; background: #AB3127">
-          ${score === 10 ? `<h4 class="remarque">&#128079; كل اجاباتك صحيحة, أنت مشجع وفي</h4>` : (score > 6) ? `<h4 class="remarque">&#9917; معرفتك عن المنتخب الجزائري جيدة</h4>` : (score > 3) ? `<h4 class="remarque">&#9917; أحسنت, لكن يمكنك تحسين معرفتك</h4>` : `<h4 class="remarque">&#128064; يمكنك تحسين معرفتك بكثير </h4>`}
+          ${
+            score === 10 ? `<h4 class="remarque">Score complet, Bravo à toi ! &#128079;</h4>` 
+            : (score > 6) ? `<h4 class="remarque">Très bon score, il te manque peu pour avoir un score complet. &#9917;</h4>` 
+            : (score > 3) ? `<h4 class="remarque">Bien joué! mais je sais que tu peux faire mieux. &#9917;</h4>` 
+            : `<h4 class="remarque">Tu peux faire beaucoup mieux que ça! &#128064;</h4>`
+          }
           <hr style="border: none; height: 2px; width:10%; background: #AB3127">
-          <h4 class="score-text"> تحصلت على ${score} إجابات صحيحة من ${quizData.length}</h4>
+          <h4 class="score-text">Ton score est de ${score} sur ${quizData.length}</h4>
           <div class="wrapper">
             <div class="circular-progress">
               <div class="value-container">${score}/${quizData.length}</div>
